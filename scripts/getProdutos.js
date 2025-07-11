823d51256313880ec0bc3284b1ec4c206d2e8376
Igor Marques <igorclash2345@gmail.com>
2025-07-11 12:56:49 -0300

    Adicionado novas funções para fetch, incluindo um novo endpoint 'clientes'

--- scripts/getProdutos.js
+++ scripts/getProdutos.js
@@ -1,6 +1,13 @@
-async function getProdutos(id) {
+// --------------------------------------------------------------------------------------------
+
+async function produtoPorId(id) {
   try {
-    const response = await fetch(`http://localhost:3000/${id}`);
+    const response = await fetch(`http://localhost:3000/produtos/${id}`);
+    if (!response.ok) {
+      throw new Error(
+        `Produto com ID ${id} não encontrado (status ${response.status})`
+      );
+    }
     const data = await response.json();
     return data;
   } catch (erro) {
@@ -9,7 +16,135 @@ async function getProdutos(id) {
   }
 }
 
-// Exemplo de uso correto:
-getProdutos(0).then((produto) => {
-  console.log(produto);
-});
+// produtoPorId(1).then((produto) => {
+//   console.log(produto);
+// });
+
+// --------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------
+
+async function buscarProdutos() {
+  try {
+    const response = await fetch("http://localhost:3000/produtos");
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error("Ocorreu um erro", err);
+    return null;
+  }
+}
+
+// buscarProdutos().then((response) => console.log(response));
+
+// --------------------------------------------------------------------------------------------
+
+async function criarProduto(nome, categoria, preco) {
+  try {
+    let objetoProduto = {
+      nome: nome,
+      categoria: categoria,
+      preco: Number(preco).toLocaleString("pt-br", {
+        style: "currency",
+        currency: "BRL",
+      }),
+    };
+    await buscarProdutos()
+      .then((dados) => {
+        let maiorId = dados.reduce((anterior, atual) => {
+          return +atual.id > anterior ? +atual.id : anterior;
+        }, 0);
+
+        Object.assign(objetoProduto, {
+          id: ++maiorId,
+        });
+        return objetoProduto;
+      })
+      .then(async (novoProduto) => {
+        try {
+          const resposta = await fetch("http://localhost:3000/produtos", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(novoProduto),
+          });
+
+          const produtoCriado = await resposta.json();
+          console.log("Produto criado:", produtoCriado);
+        } catch (erro) {
+          console.error("Erro ao criar produto:", erro.message);
+        }
+      });
+  } catch (err) {
+    console.log(err);
+  }
+}
+criarProduto("Televisão 800 polegadas", "eletrônicos", 3880.45);
+// --------------------------------------------------------------------------------------------
+
+async function produtosPorCategorias() {
+  try {
+    const response = await fetch("http://localhost:3000/produtos");
+    const data = await response.json();
+    return Object.groupBy(data, ({ categoria }) => categoria);
+  } catch (err) {
+    console.error("Ocorreu um erro", err);
+    return null;
+  }
+}
+
+// produtosPorCategorias().then((response) => console.log(response));
+
+// --------------------------------------------------------------------------------------------
+
+async function buscarClientes() {
+  try {
+    const response = await fetch("http://localhost:3000/clientes");
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.error("Ocorreu um erro", err);
+    return null;
+  }
+}
+
+async function criarCliente(nome, idade, cidade) {
+  try {
+    let objetoCliente = {
+      nome: nome,
+      idade: idade,
+      cidade: cidade,
+    };
+    await buscarClientes()
+      .then((dados) => {
+        let maiorId = dados.reduce((anterior, atual) => {
+          return +atual.id > anterior ? +atual.id : anterior;
+        }, 0);
+
+        Object.assign(objetoCliente, {
+          id: ++maiorId,
+        });
+        return objetoCliente;
+      })
+      .then(async (novoCliente) => {
+        try {
+          const resposta = await fetch("http://localhost:3000/clientes", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(novoCliente),
+          });
+
+          const clienteCriado = await resposta.json();
+          console.log("Cliente criado:", clienteCriado);
+        } catch (erro) {
+          console.error("Erro ao criar cliente:", erro.message);
+        }
+      });
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+// criarCliente("Igor", 29, "São paulo");
