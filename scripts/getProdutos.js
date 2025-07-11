91e23e9466f747181d3529062f3124ad72c55d51
Igor Marques <igorclash2345@gmail.com>
2025-07-10 23:09:20 -0300

    add função getProdutos

---
+++ scripts/getProdutos.js
@@ -0,0 +1,15 @@
+async function getProdutos(id) {
+  try {
+    const response = await fetch(`http://localhost:3000/${id}`);
+    const data = await response.json();
+    return data;
+  } catch (erro) {
+    console.error("Erro inesperado:", erro);
+    return null;
+  }
+}
+
+// Exemplo de uso correto:
+getProdutos(0).then((produto) => {
+  console.log(produto);
+});
